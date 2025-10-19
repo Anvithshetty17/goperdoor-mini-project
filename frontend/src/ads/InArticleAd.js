d01ac40863b5119b79e71ce5c652bf04ba623de1
Anvith shetty <161817374+Anvithshetty17@users.noreply.github.com>
2025-10-19 22:10:40 +0530

    Update InArticleAd.js

--- frontend/src/ads/InArticleAd.js
+++ frontend/src/ads/InArticleAd.js
@@ -1,22 +1,9 @@
 import { useEffect } from 'react';
 
 const InArticleAd = () => {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {
-      console.error('AdSense error:', e);
-    }
-  }, []);
-
+  
   return (
-    <ins className="adsbygoogle"
-      style={{ display: 'block', textAlign: 'center' }}
-      data-ad-layout="in-article"
-      data-ad-format="fluid"
-      data-ad-client="ca-pub-4093862221318795"
-      data-ad-slot="5378504439"
-    />
+   <></>
   );
 };
 
