ec38a932b8f89592a26c5dd0214b1aad22ca2414
Anvith shetty <161817374+Anvithshetty17@users.noreply.github.com>
2025-10-19 22:10:20 +0530

    Remove Adsense code from FluidCustomAd component
    Removed Adsense integration and returned an empty fragment.

--- frontend/src/ads/FluidCustomAd.js
+++ frontend/src/ads/FluidCustomAd.js
@@ -1,23 +1,14 @@
 import { useEffect } from 'react';
 
 const FluidCustomAd = () => {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {
-      console.error('Adsense error', e);
-    }
-  }, []);
 
-  return (
-    <ins className="adsbygoogle"
-      style={{ display: 'block',padding:'10px' }}
-      data-ad-format="fluid"
-      data-ad-layout-key="-fb+5w+4e-db+86"
-      data-ad-client="ca-pub-4093862221318795"
-      data-ad-slot="7366373522"
-    />
+
+  
+    return (
+   <></>
   );
+   
+  
 };
 
 export default FluidCustomAd;
